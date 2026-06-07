fd4aa6913becc6a923cd33861d5b24ca611a9472
nikitaaaa123 <nbhansali2006@gmail.com>
2026-06-08 01:01:14 +0530

    a

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.json());
 
 app.use('/api', profileRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
